f2eaa28b4687cbad2f1006de9ef14364a2617887
Victor Sedat <vsedat@z2r4p5.42lyon.fr>
2021-11-09 22:40:34 +0100

    UwU

--- ft_split.c
+++ ft_split.c
@@ -5,98 +5,72 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsedat <vsedat@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/05 12:28:01 by vsedat            #+#    #+#             */
-/*   Updated: 2021/11/09 10:52:35 by vsedat           ###   ########lyon.fr   */
+/*   Created: 2021/11/09 19:31:58 by vsedat            #+#    #+#             */
+/*   Updated: 2021/11/09 22:38:18 by vsedat           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	c_words(const char *str, char sep)
+int	nbwords(const char *str, char c)
 {
-	int	nbwords;
-	int	cs;
-	int	lenword;
-
-	nbwords = 0;
-	cs = 0;
-	lenword = 0;
-	while (str[cs])
-	{
-		if (str[cs] == sep || (!str[cs + 1] && str[cs] != sep))
-		{
-			if (lenword != 0 || (!str[cs + 1] && str[cs] != sep))
-				nbwords++;
-			lenword = 0;
-		}
-		else
-			lenword++;
-		cs++;
-	}
-	return (nbwords);
-}
-
-int	lenword(const char *str, char sep, int whichword)
-{
-	int	cs;
-	int	lenword;
-
-	cs = 0;
-	while (whichword != 0 && str[cs])
-	{
-		lenword = 0;
-		while (str[cs] == sep && str[cs])
-			cs++;
-		while (str[cs] != sep && str[cs])
-		{
-			cs++;
-			lenword++;
-		}
-		whichword--;
-	}
-	return (lenword);
-}
-
-char	**mallocerror(char **tab)
-{
-	unsigned int	i;
+	int	i;
+	int	isword;
 
 	i = 0;
-	while (tab[i])
+	isword = 0;
+	while (*str)
 	{
-		free(tab[i]);
-		i++;
+		if (*str != c && isword == 0)
+		{
+			isword = 1;
+			i++;
+		}
+		else if (*str == c)
+			isword = 0;
+		str++;
 	}
-	free(tab);
-	return (0);
+	return (i);
+}
+
+char	*getword(char const *s, int start, int stop)
+{
+	char	*split;
+	int		i;
+
+	i = 0;
+	split = malloc((stop - start + 1) * sizeof(char));
+	while (start < stop)
+		split[i++] = s[start++];
+	split[i] = 0;
+	return (split);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	int		i;
-	char	**split;
 	int		j;
-	int		k;
+	int		start;
+	char	**split;
 
 	if (!s)
 		return (0);
-	split = malloc((c_words(s, c) + 1) * sizeof(char *));
+	split = malloc((nbwords(s, c) + 1) * sizeof(char *));
 	if (!split)
 		return (0);
-	k = 0;
-	i = 0;
-	while (i < c_words(s, c))
+	i = -1;
+	j = 0;
+	start = -1;
+	while (++i <= ft_strlen(s))
 	{
-		split[i] = malloc((lenword(s, c, i)) * sizeof(char));
-		if (!split[i])
-			return (0);
-		j = 0;
-		while (s[k] == c)
-			k++;
-		while (j < lenword(s, c, i + 1))
-			split[i][j++] = s[k++];
-		split[i++][j] = 0;
+		if (s[i] != c && start < 0)
+			start = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && start >= 0)
+		{
+			split[j++] = getword(s, start, i);
+			start = -1;
+		}
 	}
-	split[i] = 0;
+	split[j] = 0;
 	return (split);
 }
