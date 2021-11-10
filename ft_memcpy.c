f482a37df00869473f84da594f8da24e6bf270b8
Victor Sedat <vsedat@z2r1p3.42lyon.fr>
2021-11-10 18:21:31 +0100

    help me

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: vsedat <vsedat@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 10:19:44 by vsedat            #+#    #+#             */
-/*   Updated: 2021/11/03 15:39:14 by vsedat           ###   ########lyon.fr   */
+/*   Updated: 2021/11/10 18:19:45 by vsedat           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	d = dst;
 	s = src;
 	i = 0;
-	if (!src || !dst)
+	if (!src && !dst)
 		return (0);
 	while (i < n)
 	{
